66b53830e967298c1a3a87d355a1f0bd2c742a2d
samik shk <samik.shakya8848@hotmail.com>
2021-08-25 19:57:45 +0545

    asdfasdf

--- Store_management_system/Form2.cs
+++ Store_management_system/Form2.cs
@@ -90,8 +90,13 @@ namespace Store_management_system
                     this.Refresh();
                     //i commeted on al ine
                     //2 nd test git hub
+<<<<<<< HEAD
                     //alsjdfklajsdfklj
                     //asdfasdfasdf
+=======
+                    //3rd comment
+                    //4th comment test
+>>>>>>> b851b96602f78a9582d07bddbefe68781dca1c3f
                 }
             }
         }
